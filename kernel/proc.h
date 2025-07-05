53e10b8ee189b4756a15eb040616602837e1c906
dorsarz <dorsarezaei24@gmail.com>
2025-07-05 19:48:43 +0330

    part 2 done

--- kernel/proc.h
+++ kernel/proc.h
@@ -109,3 +109,23 @@ the process
 struct thread *current_thread; // Pointer to the currently
 running thread
 };
+
+struct thread
+{
+  enum threadstate state;
+  struct trapframe *trapframe; 
+  uint id;
+  uint join;
+  int sleep_n;
+  uint sleep_tick0;
+  
+};
+
+enum threadstate{
+   THREAD_UNUSED,
+   THREAD_RUNNABLE,
+   THREAD_RUNNING,
+   THREAD_JOINED,
+   THREAD_SLEEPING
+};
+
