9b61402a75e489f4f5f9ebffd93e99a2ef7975d9
parnianshamsa <153368555+parnianshamsa@users.noreply.github.com>
2025-07-05 19:45:39 +0330

    Update proc.h

--- kernel/proc.h
+++ kernel/proc.h
@@ -104,9 +104,5 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
-  struct thread threads[NTHREAD]; // Array of threads belonging to
-the process
-struct thread *current_thread; // Pointer to the currently
-running thread
+  
 };
-
