515ad92762502fd32268ae129891a607abec059e
dorsarz <dorsarezaei24@gmail.com>
2025-07-13 15:24:10 +0330

    last changes

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -101,18 +101,43 @@ sys_trigger(void)
   return 0;
 }
 
-uint64
- sys_thread(void) {
-uint64 start_thread, stack_address, arg;
-argaddr(0, &start_thread);
-argaddr(1, &stack_address);
-argaddr(2, &arg);
-struct thread *t = allocthread(start_thread, stack_address, arg);
-return t ? t->id : 0;
-}
+uint64 sys_thread(void) {
+  uint64 start_thread, stack_address, arg;
+  argaddr(0, &start_thread);
+  argaddr(1, &stack_address);
+  argaddr(2, &arg);
+  struct thread *t = allocthread(start_thread, stack_address, arg);
+  return t ? t->id : 0;
+  }
 
 uint64 sys_jointhread(void) {
-int id;
-argint(0, &id);
-return jointhread(id);
+    int id;
+    argint(0, &id);
+    return jointhread(id);
+    }
+
+uint64
+sys_sleep(void)
+{
+int n;
+uint ticks0;
+argint(0, &n);
+if(n < 0)
+n = 0;
+acquire(&tickslock);
+ticks0 = ticks;
+if (myproc()->current_thread) {
+release(&tickslock);
+sleepthread(n, ticks0);
+return 0;
+}
+while(ticks - ticks0 < n){
+if(killed(myproc())){
+release(&tickslock);
+return -1;
+}
+sleep(&ticks, &tickslock);
+}
+release(&tickslock);
+return 0;
 }
